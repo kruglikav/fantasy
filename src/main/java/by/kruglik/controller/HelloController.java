a5e686023c94909c067b60cf3614e81182e8ad1e
kruglik <kruglikav@gmail.com>
2014-11-11 10:22:19 +0300

    init spring security

--- src/main/java/by/kruglik/controller/HelloController.java
+++ src/main/java/by/kruglik/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(){
+    public String hello1(){
         return "home";
     }
 }
